9748e6c7e42def58b6c03bf0c0f4d8839bf4f18f
Michael Markert <markert.michael@googlemail.com>
2013-08-03 15:47:14 +0200

    Initial commit.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

---
+++ ahp.el
@@ -0,0 +1,32 @@
+;;; ahp.el --- ad hoc projects
+
+;; Copyright (C) 2013 Michael Markert
+;; Author: Michael Markert <markert.michael@gmail.com>
+;; Created: 2013-08-02
+;; Version:
+;; Keywords: projects convenicence
+;; URL: https://github.com/cofi/ahp
+;;
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'ahp)
+;;; ahp.el ends here
